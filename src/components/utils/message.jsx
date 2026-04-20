25fb4dc4eda166f6b3159a991f0899331b7a30d3
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-04-20 11:45:17 +0300

    Update message.jsx

--- src/components/utils/message.jsx
+++ src/components/utils/message.jsx
@@ -1,7 +1,12 @@
 import { messages } from "../../lib/messages.js"
 
 const message = async (params) => {
-    let lang = localStorage.getItem('blogs8787-language')
+    let lang = 'ru'
+    if (!params.language) {
+        lang = localStorage.getItem('blogs8787-language')
+    } else {
+        lang = params.language
+    }
     return await messages[params.id].value({
         lang,
         data: params.data
