89f5db9bbdcec0c722648b573e3a56951c86f833
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-04-21 21:39:26 +0300

    Update and rename blogs.jsx to blogList.jsx

---
+++ src/components/blog/blogList.jsx
@@ -0,0 +1,10 @@
+import { newMessage, genMessage, customMessage } from '../utils/message.jsx'
+import BLOG from './blogInList.jsx'
+
+const BlogList = () => {
+  return (
+    <div class='blogs'></div>
+  )
+}
+
+export default BlogList
