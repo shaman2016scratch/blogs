560ea32045ad1eb704306cd289966ebf2a46656c
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-04-21 23:13:12 +0300

    Update about.jsx

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -2,12 +2,14 @@ import { newMessage } from '../../components/utils/message.jsx'
 
 const AboutBlogs = () => {
     return (
-        <h1>{newMessage({
-            id: 'aboutPage.header.aboutService'
-        })}</h1>
-        <p>{newMessage({
-            id: 'pages.main.about.main'
-        })}</p>
+        <div>
+            <h1>{newMessage({
+                id: 'aboutPage.header.aboutService'
+            })}</h1>
+            <p>{newMessage({
+                id: 'pages.main.about.main'
+            })}</p>
+        </div>
     )
 }
 
