ce39afe5c938b0666801cc9429ecda5d7f5606dd
shaman2016scratch <g.ilkh@yandex.ru>
2026-05-04 01:52:30 +0300

    Upd

--- src/components/blog/blogList.jsx
+++ src/components/blog/blogList.jsx
@@ -1,12 +1,34 @@
-'use server'
+import { useState, useEffect } from 'react'
 import { newMessage, genMessage, customMessage, Message } from '../utils/message.jsx'
 import BLOG from './blogInList.jsx'
 import blogs8787api from 'blogs8787api.js'
 
 const BlogList = async ({type}) => {
+    const [recommendations, setRecommendations] = useState([]);
+    const [loading, setLoading] = useState(true);
     const isInUser = type === 'inUser'
     const isInRecommendations = type === 'inRecommendations'
-    const recomendations = (await blogs8787api.recomendations()).list
+
+    useEffect(() => {
+        const loadData = async () => {
+            if (!isInRecommendations) {
+                setLoading(false);
+                return;
+            }
+            try {
+                const response = await blogs8787api.recomendations();
+                setRecommendations(response?.list || []);
+            } catch (error) {
+                console.error('Error loading recommendations:', error);
+                setRecommendations([]);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        loadData();
+    }, [isInRecommendations]);
+
     return (
         <div class='blogs'>
             {isInUser ? (
@@ -14,9 +36,15 @@ const BlogList = async ({type}) => {
             ) : null}
             {isInRecommendations ? (
                 <div>
-                    {recomendations.map((blog) => (
-                        <BLOG id={blog.id} name={blog.name} author={blog.author} />
-                    ))}
+                    {loading ? (
+                        <p>Loading</p>
+                    ) : (
+                        <div>
+                            {recomendations.map((blog) => (
+                                <BLOG id={blog.id} name={blog.name} author={blog.author} />
+                            ))}
+                        </div>
+                    )}
                 </div>
             ) : null}
         </div>
